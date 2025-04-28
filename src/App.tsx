f03551420dd24cb489393bdb12d47c1804a8b5d2
xSkyLite <chkalovua11@gmail.com>
2025-04-28 21:24:22 +0300

    develop1

--- src/App.tsx
+++ src/App.tsx
@@ -15,8 +15,6 @@ export const goodsFromServer = [
   'Garlic',
 ];
 
-// 123
-
 const sortMethods = {
   abc: (a: string, b: string) => a.localeCompare(b),
   length: (a: string, b: string) => a.length - b.length,
